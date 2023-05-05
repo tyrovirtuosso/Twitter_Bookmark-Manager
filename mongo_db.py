e0b42e746a30b5eeb3930077f904c818bacc8cbb
tyrovirtuosso <tyrovirtuosso@proton.me>
2023-05-05 11:11:59 +0530

    set enviroment variable for clusted url

--- mongo_db.py
+++ mongo_db.py
@@ -1,8 +1,11 @@
 from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 class MongoDB():
     def __init__(self) -> None:
-        cluster = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.8.2"    
+        cluster = os.environ.get("MONGODB_CLUSTER")
         self.client = MongoClient(cluster)
      
     def check_collection(self, db_name, collection_name):
